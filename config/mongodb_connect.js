26856a7c4dd76a8cb2328aef7745dff81e45acc9
Vinay Phatarpekar <vinayp9399@gmail.com>
2024-06-30 13:16:43 +0530

    1234

--- config/mongodb_connect.js
+++ config/mongodb_connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/Clinic",{});
+mongoose.connect("mongodb://127.0.0.1:27017/Clinic",{});
 const usersSchema = {
     name:String,
     email:String,
