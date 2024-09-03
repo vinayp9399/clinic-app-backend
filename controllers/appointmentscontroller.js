019191e47107a070a51789fe8917b0345d077dd8
Vinay Phatarpekar <vinayp9399@gmail.com>
2024-09-03 22:02:45 +0530

    done

--- controllers/appointmentscontroller.js
+++ controllers/appointmentscontroller.js
@@ -62,7 +62,7 @@ exports.findstatus = async(request, response) =>{
 
 exports.docfollowups = async(request, response) =>{
     let date = new Date;
-    let datefollow = String(date).slice(0,15)
+    let datefollow = `${date.getFullYear()}-0${date.getMonth()+1}-0${date.getDate()}`
     const result = await appointments.find({doctorid:request.params.doctorid, followupdate:datefollow})
     response.send(JSON.stringify({'error':'','message':result}))
 }
